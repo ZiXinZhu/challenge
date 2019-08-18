eda58937ceb12e14521cfab103feeb7627e704b7
husky <1101648204@qq.com>
2019-08-18 17:15:04 +0800

    execel

---
+++ src/main/java/com/zzx/insert/thread/safe/Communication.java
@@ -0,0 +1,32 @@
+package com.zzx.insert.thread.safe;
+
+import java.util.concurrent.*;
+
+public class Communication{
+
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        ExecutorService executorService= Executors.newFixedThreadPool(5);
+        Future future=executorService.submit(Communication::call);
+        try {
+            //TODO Callable超时时间
+            System.out.println(future.get(3,TimeUnit.SECONDS).toString());
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+        System.out.println("结束");
+    }
+
+
+    private static Long call() throws Exception {
+        long sum=0L;
+        for (int i=0;i<7;i++){
+            Thread.sleep(300);
+            sum=sum+i;
+        }
+
+        System.out.println("循环结束");
+        return sum;
+    }
+}
--- src/main/java/com/zzx/insert/thread/safe/DoThreadLocal.java
+++ src/main/java/com/zzx/insert/thread/safe/DoThreadLocal.java
@@ -7,11 +7,17 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 public class DoThreadLocal implements Runnable {
     private static ThreadLocal<String> local = new ThreadLocal<>();
-    private static ThreadLocal<ConcurrentHashMap<String,String>> locals = new ThreadLocal<>();
-    private static ThreadLocal<List<String>> locales = new ThreadLocal<>();
+    //TODO 代替同步Map
+    private static ThreadLocal<ConcurrentHashMap<String, String>> localMap = new ThreadLocal<>();
+    //TODO 代替同步List
+    private static ThreadLocal<CopyOnWriteArrayList< String>> localList = new ThreadLocal<>();
+    //TODO 代替同步Set
+    private static ThreadLocal<CopyOnWriteArraySet< String>> localSet = new ThreadLocal<>();
 
     @Override
     public void run() {
@@ -30,13 +36,15 @@ public class DoThreadLocal implements Runnable {
 
     public static void main(String[] args) {
         //TODO 判断字符串非空（org.springframework.util.StringUtils）
-        String string="z";
-        boolean empty=StringUtils.isEmpty(string);
+        String string = "z";
+        boolean empty = StringUtils.isEmpty(string);
         System.out.println(String.valueOf(empty));
         //TODO 判断集合是否为空（需要导入依赖commons-collections4）
-        Collection<String> collection=new ArrayList<>();
-        boolean b=CollectionUtils.isEmpty(collection);
+        Collection<String> collection = new ArrayList<>();
+        boolean b = CollectionUtils.isEmpty(collection);
         System.out.println(String.valueOf(b));
-//      new Thread(new DoThreadLocal()).start();
+        System.out.println(String.valueOf(System.nanoTime()));
+        System.out.println(String.valueOf(System.currentTimeMillis()));
+        new Thread(new DoThreadLocal()).start();
     }
 }
