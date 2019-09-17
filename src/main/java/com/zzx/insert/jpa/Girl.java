8af8f38aade58a1bb94f32d5368804460761bd97
husky <1101648204@qq.com>
2019-09-17 15:42:27 +0800

    JPA增删改插

---
+++ src/main/java/com/zzx/insert/jpa/Girl.java
@@ -0,0 +1,53 @@
+package com.zzx.insert.jpa;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "girl")
+public class Girl {
+
+
+  @Id
+  @Column(length = 9)
+  private int id;
+  @Column
+//  @Column(name = "age",columnDefinition = "int(9) DEFAULT NULL comment 'age' ")
+  private int age;
+  @Column
+//  @Column(name = "high",columnDefinition = "int(9) DEFAULT NULL comment 'high' ")
+  private int high;
+
+
+  public int getId() {
+    return id;
+  }
+
+  public Girl setId(int id) {
+    this.id = id;
+    return this;
+  }
+
+
+  public int getAge() {
+    return age;
+  }
+
+  public Girl setAge(int age) {
+    this.age = age;
+    return this;
+  }
+
+
+  public int getHigh() {
+    return high;
+  }
+
+  public Girl setHigh(int high) {
+    this.high = high;
+    return this;
+  }
+
+}
---
+++ src/main/java/com/zzx/insert/jpa/JpaGirlDao.java
@@ -0,0 +1,34 @@
+package com.zzx.insert.jpa;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+
+
+@Repository
+public interface JpaGirlDao extends JpaRepository<Girl,Integer>  {
+
+    @Query(nativeQuery = true, value = "SELECT * FROM  girl where girl.age=?1")
+    List<Girl> findByAge(int age);
+
+    @Transactional
+    @Modifying
+    @Query("update Girl set high=?2 where age=?1")
+    int  updateGirl(int age,int high);
+
+
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true,value = "delete from  girl  where age=?1")
+//    @Query("delete from  Girl  where age=?1")
+    int deleteByAge(int age);
+
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true,value = "insert into girl (age, high)  values (?1,?2)")
+    int addGirl(int age,int high);
+}
